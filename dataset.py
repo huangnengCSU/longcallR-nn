61cc128c7a41a43bfe5bfc93f036582414c0013c
Neng Huang <neng@ds.dfci.harvard.edu>
2024-07-08 18:52:07 -0400

    init

---
+++ dataset.py
@@ -0,0 +1,245 @@
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import argparse
+import os
+
+
+# import tables
+# from options import gt_decoded_labels, zy_decoded_labels
+
+# TABLE_FILTERS = tables.Filters(complib='blosc:lz4hc', complevel=5)
+# shuffle_bin_size = 50000
+#
+# no_flanking_bases = 16
+# no_of_positions = 2 * no_flanking_bases + 1
+# channel = ('A', 'C', 'G', 'T', 'I', 'I1', 'D', 'D1', '*',
+#            'a', 'c', 'g', 't', 'i', 'i1', 'd', 'd1', '#')
+# channel_size = len(channel)
+# ont_input_shape = input_shape = [no_of_positions, channel_size]
+# label_shape = [21, 3, no_of_positions, no_of_positions]
+# label_size = sum(label_shape)
+#
+#
+# def calculate_percentage(ts):
+#     # ts: L, N, C
+#     # return: L, N, 5
+#     ts_A = np.expand_dims(((ts == 1).sum(2) / ((ts != -2).sum(2) + 1e-9)), 2)
+#     ts_C = np.expand_dims(((ts == 2).sum(2) / ((ts != -2).sum(2) + 1e-9)), 2)
+#     ts_G = np.expand_dims(((ts == 3).sum(2) / ((ts != -2).sum(2) + 1e-9)), 2)
+#     ts_T = np.expand_dims(((ts == 4).sum(2) / ((ts != -2).sum(2) + 1e-9)), 2)
+#     ts_D = np.expand_dims(((ts == -1).sum(2) / ((ts != -2).sum(2) + 1e-9)), 2)
+#     return np.concatenate((ts_A, ts_C, ts_G, ts_T, ts_D), axis=2)
+
+
+# def balance_dataset(gt_label, zy_label):
+#     """
+#     position_matrix.shape   (495703, 33, 18)
+#     gt_label.shape  (495703,)
+#     zy_label.shape  (495703,)
+#     """
+#     num_gts = len(gt_decoded_labels)
+#     num_zys = len(zy_decoded_labels)
+#     indexes_for_gt_zy = {}
+#     num_of_max_categories = 0
+#     for i in range(num_gts):
+#         for j in range(num_zys):
+#             findex = np.where((gt_label == i) & (zy_label == j))[0]
+#             if len(findex) >= num_of_max_categories:
+#                 num_of_max_categories = len(findex)
+#             indexes_for_gt_zy[(i, j)] = findex
+#
+#     # 每个类上采样
+#     non_zero_categories = 0
+#     for k in indexes_for_gt_zy.keys():
+#         size_of_categories = len(indexes_for_gt_zy[k])
+#         if size_of_categories > 0 and size_of_categories < num_of_max_categories:
+#             sample_num = num_of_max_categories - size_of_categories
+#             sampling_index = np.random.choice(indexes_for_gt_zy[k], size=sample_num, replace=True)
+#             indexes_for_gt_zy[k] = indexes_for_gt_zy[k].tolist() + sampling_index.tolist()
+#             non_zero_categories += 1
+#
+#     total_indexes = []
+#     for k in indexes_for_gt_zy.keys():
+#         total_indexes.extend(indexes_for_gt_zy[k])
+#
+#     # 所有类的总和随机下采样
+#     np.random.shuffle(total_indexes)
+#     final_index = np.random.choice(total_indexes, size=int(len(total_indexes) / non_zero_categories))
+#     return final_index
+#
+#
+# def filter_sample(position_matrix, gt_label, zy_label):
+#     pass
+
+
+# class TrainDataset(Dataset):
+#     def __init__(self, datapath, use_balance=False, for_evaluate=False):
+#         # tables.set_blosc_max_threads(16)
+#         table_file = tables.open_file(datapath, 'r')
+#         position_matrix = np.array(table_file.root.position_matrix)  # [N,33,18]
+#         label = table_file.root.label  # [N,90]
+#         gt_label = np.array(label[:, :21].argmax(1))  # [N]
+#         zy_label = np.array(label[:, 21:24].argmax(1))  # [N]
+#         indel1_label = np.array(label[:, 24:57].argmax(1))  # [N]
+#         indel2_label = np.array(label[:, 57:].argmax(1))  # [N]
+#         table_file.close()
+#         if use_balance:
+#             train_idx = balance_dataset(gt_label=gt_label, zy_label=zy_label)
+#             self.position_matrix = position_matrix[train_idx]
+#             self.gt_label = gt_label[train_idx]
+#             self.zy_label = zy_label[train_idx]
+#             self.indel1_label = indel1_label[train_idx]
+#             self.indel2_label = indel2_label[train_idx]
+#         else:
+#             self.position_matrix = position_matrix
+#             self.gt_label = gt_label
+#             self.zy_label = zy_label
+#             self.indel1_label = indel1_label
+#             self.indel2_label = indel2_label
+#
+#         if for_evaluate:
+#             variant_idx = np.where(self.zy_label > 0)[0]  # zy_decoded_labels = ['0/0', '1/1', '0/1']
+#             self.position_matrix = self.position_matrix[variant_idx]
+#             self.gt_label = self.gt_label[variant_idx]
+#             self.zy_label = self.zy_label[variant_idx]
+#             self.indel1_label = self.indel1_label[variant_idx]
+#             self.indel2_label = self.indel2_label[variant_idx]
+#
+#     def __getitem__(self, i):
+#         position_matrix = self.position_matrix[i]
+#         gt_label = self.gt_label[i]
+#         zy_label = self.zy_label[i]
+#         indel1_label = self.indel1_label[i]
+#         indel2_label = self.indel2_label[i]
+#         return position_matrix, gt_label, zy_label, indel1_label, indel2_label
+#
+#     def __len__(self):
+#         return len(self.gt_label)
+
+
+class TrainDataset(Dataset):
+    def __init__(self, data_paths, flanking_size):
+        ## data_paths: list of file paths
+        training_matrix = []
+        label = []
+        for datapath in data_paths:
+            with open(datapath, 'r') as fin:
+                single_feature_matrix = []
+                idx = 0
+                for line in fin:
+                    idx += 1
+                    if idx <= flanking_size * 2 + 1:
+                        line = [float(v) for v in line.strip().rstrip(',').split(',')]
+                        single_feature_matrix.append(line)
+                    elif idx == flanking_size * 2 + 2:
+                        label.append(int(line.strip().split('\t')[0]))
+                        training_matrix.append(single_feature_matrix)
+                        single_feature_matrix = []
+                        idx = 0
+        assert len(label) > 0
+        nfeatures = len(training_matrix[0][0])
+        self.training_matrix = np.array(training_matrix).reshape(-1, flanking_size * 2 + 1, nfeatures)  # [N,41,38]
+        assert len(label) == len(self.training_matrix)
+        self.label = np.array(label)  # [N,]
+
+    def __getitem__(self, i):
+        feature_matrix = self.training_matrix[i]
+        label = self.label[i]
+        return feature_matrix, label
+
+    def __len__(self):
+        return len(self.label)
+
+
+# class PredictDataset(Dataset):
+#     def __init__(self, datapath):
+#         # tables.set_blosc_max_threads(16)
+#         table_file = tables.open_file(datapath, 'r')
+#         position_matrix = np.array(table_file.root.position_matrix)  # [N,33,18]
+#         position = table_file.root.position
+#         positions = []
+#         reference_bases = []
+#         contig_names = []
+#         for item in position:
+#             ctg_name, pos, seq = str(item[0], encoding="utf-8").strip().split(':')
+#             contig_names.append(ctg_name)
+#             pos = int(pos)
+#             positions.append(pos)
+#             reference_bases.append(ord(seq[16]))  # ASCII
+#         positions = np.array(positions)
+#         reference_bases = np.array(reference_bases)
+#         table_file.close()
+#         self.contig_names = contig_names
+#         self.position_matrix = position_matrix
+#         self.positions = positions
+#         self.reference_bases = reference_bases
+#
+#     def __getitem__(self, i):
+#         contig_names = self.contig_names[i]
+#         positions = self.positions[i]
+#         reference_bases = self.reference_bases[i]
+#         position_matrix = self.position_matrix[i]
+#         return contig_names, positions, reference_bases, position_matrix
+#
+#     def __len__(self):
+#         return len(self.position_matrix)
+
+
+class PredictDataset(Dataset):
+    def __init__(self, data_paths, flanking_size):
+        predict_matrix = []
+        positions = []
+        for datapath in data_paths:
+            with open(datapath, 'r') as fin:
+                idx = 0
+                single_feature_matrix = []
+                for line in fin:
+                    idx += 1
+                    if idx <= flanking_size * 2 + 1:
+                        line = [float(v) for v in line.strip().rstrip(',').split(',')]
+                        single_feature_matrix.append(line)
+                    elif idx == flanking_size * 2 + 2:
+                        positions.append(line.strip())
+                        predict_matrix.append(single_feature_matrix)
+                        single_feature_matrix = []
+                        idx = 0
+        assert len(positions) > 0
+        nfeatures = len(predict_matrix[0][0])
+        self.predict_matrix = np.array(predict_matrix).reshape(-1, flanking_size * 2 + 1, nfeatures)  # [N,41,38]
+        assert len(positions) == len(self.predict_matrix)
+        self.positions = positions  # [N,]
+
+    def __getitem__(self, i):
+        position = self.positions[i]
+        feature_matrix = self.predict_matrix[i]
+        return position, feature_matrix
+
+    def __len__(self):
+        return len(self.predict_matrix)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-data', required=True, help='directory of bin files')
+    opt = parser.parse_args()
+
+    epoch = 0
+    filepaths = [opt.data + '/' + file for file in os.listdir(opt.data)]
+    dataset = TrainDataset(data_paths=filepaths, flanking_size=20)
+    while epoch < 10:
+        dl = DataLoader(dataset, batch_size=2000, shuffle=True)
+        for batch in dl:
+            feature_matrices, labels = batch
+            print("Epoch ", epoch, ":", feature_matrices.shape)
+        epoch += 1
+
+    """
+    filepaths = [opt.data + '/' + file for file in os.listdir(opt.data)]
+    dataset = PredictDataset(data_paths=filepaths, flanking_size=20)
+    while epoch < 10:
+        dl = DataLoader(dataset, batch_size=2000, shuffle=False)
+        for batch in dl:
+            positions, feature_matrices = batch
+            print("Epoch ", epoch, ":", feature_matrices.shape)
+        epoch += 1
+    """
---
+++ predict.py
@@ -0,0 +1,78 @@
+import argparse
+import os
+import yaml
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from math import log, e
+from nn import LSTMNetwork
+from dataset import PredictDataset
+from utils import AttrDict
+
+
+def calculate_score(probability):
+    p = probability
+    tmp = max((-10 * log(e, 10)) * log(((1.0 - p) + 1e-300) / (p + 1e-300)) + 10, 0)
+    return float(round(tmp, 2))
+
+
+def calculate_phred_scores(probs):
+    """
+    Calculate Phred scores for an array of probabilities.
+
+    Parameters:
+    probs (array-like): An array of error probabilities.
+
+    Returns:
+    np.ndarray: An array of Phred quality scores.
+    """
+    # Convert probabilities to Phred scores using the formula Q = -10 * log10(P)
+    phred_scores = -10 * np.log10(probs)
+    return phred_scores
+
+
+def predict(model, test_dataset, batch_size, output_file, device):
+    model.eval()
+    dl = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    fout = open(output_file, 'w')
+    for batch in dl:
+        positions, feature_matrices = batch
+        feature_tensor = feature_matrices.type(torch.FloatTensor).to(device)
+
+        zy_output_ = model.predict(feature_tensor)
+        zy_output_ = zy_output_.detach().cpu().numpy()
+        zy_prob = np.max(zy_output_, axis=1)  # [N]
+        zy_output = np.argmax(zy_output_, axis=1)  # [N]
+        zy_qual = calculate_phred_scores(1 - zy_prob)
+
+        ## write to csv file
+        ## position, zygosity, quality
+        for i in range(len(positions)):
+            fout.write(positions[i] + ',' + str(zy_output[i]) + ',' + str(zy_qual[i]) + '\n')
+    fout.close()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-config', type=str, required=True, help='path to config file')
+    parser.add_argument('-model_path', required=True, help='path to trained model')
+    parser.add_argument('-data', required=True, help='directory of feature files')
+    # parser.add_argument('-contig', required=True, help='contig name of the input bin files')
+    parser.add_argument('-output', required=True, help='output vcf file')
+    parser.add_argument('-batch_size', type=int, default=1000, help='batch size')
+    parser.add_argument('--no_cuda', action="store_true", help='If running on cpu device, set the argument.')
+    opt = parser.parse_args()
+    device = torch.device('cuda' if not opt.no_cuda else 'cpu')
+    configfile = open(opt.config)
+    config = AttrDict(yaml.load(configfile, Loader=yaml.FullLoader))
+    pred_model = LSTMNetwork(config.model).to(device)
+    checkpoint = torch.load(opt.model_path, map_location=device)
+    pred_model.encoder.load_state_dict(checkpoint['encoder'])
+    pred_model.forward_layer.load_state_dict(checkpoint['forward_layer'])
+    testing_paths = [opt.data + '/' + fname for fname in os.listdir(opt.data)]
+    predict_dataset = PredictDataset(testing_paths, config.data.flanking_size)
+    predict(pred_model, predict_dataset, opt.batch_size, opt.output, device)
+
+
+if __name__ == '__main__':
+    main()
