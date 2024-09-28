ab39c8483d3e68d01d032617ef9179f59cd2f646
neng <neng@ds.dfci.harvard.edu>
2024-09-27 20:29:28 -0400

    fix install

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 version = {}
 with open("longcallR_nn/_version.py") as version_file:
@@ -12,13 +12,13 @@ setup(
     author_email="neng@ds.dfci.harvard.edu",
     long_description="longcallR: a deep learning based variant caller for long-reads RNA-seq data",
     url="https://github.com/huangnengCSU/longcallR-nn",
-    download_url='https://github.com/huangnengCSU/longcallR-nn/archive/{}.tar.gz'.format(version),
+    download_url='https://github.com/huangnengCSU/longcallR-nn/archive/{}.tar.gz'.format(version['__version__']),
     entry_points={
-            'console_scripts': [
-                'longcallR_nn=longcallR_nn:main',
-                ],
-            },
+        'console_scripts': [
+            'longcallR_nn=longcallR_nn.longcallR_nn:main',
+        ],
+    },
     platforms="Unix like",
     zip_safe=False,
-    packages=["."],
+    packages=find_packages(),  # Make sure this includes all packages
 )
